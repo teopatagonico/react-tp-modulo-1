0f5c1d57e3ed2cdf86c54cc07cad054525413268
Teo Moreno Piccini <teo.patagonico1.apps@gmail.com>
2025-06-06 18:44:24 -0300

    Implementado el componente Formulario utilizando el componente Campo (version 1)

---
+++ src/componentes/Formulario.jsx
@@ -0,0 +1,17 @@
+import Campo from "./Campo"
+
+function Formulario() {
+    return (
+        <form>
+            <Campo name="Nombre" type="text"/>
+            <Campo name="Apellido" type="text"/>
+            <Campo name="Email" type="text"/>
+            <Campo name="Teléfono" type="number"/>
+            <Campo name="Contraseña" type="password"/>
+            <Campo name="Confirmar contraseña" type="password"/>
+            <input type="submit" value="Registrarse"/>
+        </form>
+    )
+}
+
+export default Formulario
